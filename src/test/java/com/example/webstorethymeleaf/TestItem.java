3750bd0e7992486e3689504044123168e6682c7b
marcusH3 <marcus.hurtigh@outlook.com>
2023-04-26 22:41:17 +0200

    Tests for item and addcustomer addded. item.html also added but missing correct template.

---
+++ src/test/java/com/example/webstorethymeleaf/TestItem.java
@@ -0,0 +1,13 @@
+package com.example.webstorethymeleaf;
+
+
+import com.example.webstorethymeleaf.POJO.Item;
+
+
+public class TestItem extends Item {
+
+    public TestItem() {
+        super();
+    }
+}
+
--- src/test/java/com/example/webstorethymeleaf/WebStoreThymeleafApplicationTests.java
+++ src/test/java/com/example/webstorethymeleaf/WebStoreThymeleafApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.webstorethymeleaf;
 
 import com.example.webstorethymeleaf.POJO.Customer;
+import com.example.webstorethymeleaf.POJO.Item;
 import com.example.webstorethymeleaf.Repositories.CustomerRepo;
+import com.example.webstorethymeleaf.Repositories.ItemRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
@@ -35,6 +37,10 @@ class WebStoreThymeleafApplicationTests {
 
 	@MockBean
 	private CustomerRepo customerRepo;
+
+	@MockBean
+	private ItemRepo itemRepo;
+
 
 	@Test
 	public void testGetAllCustomer() throws Exception {
@@ -71,7 +77,6 @@ class WebStoreThymeleafApplicationTests {
 				.andExpect(view().name("customer.html"))
 				.andExpect(model().attribute("customer", customer));
 	}
-
 	@Test
 	public void  deleteCustomerById() throws Exception {
 		// Prepare mock data
@@ -103,6 +108,113 @@ class WebStoreThymeleafApplicationTests {
 		// Verify that deleteById was called with the correct ID
 		verify(customerRepo).deleteById(2L);
 	}
-	/*CATEGORY CONTROLLER TESTING*/
+	@Test
+	public void testAddCustomer() throws Exception {
+		// Prepare mock data
+		Customer newCustomer = new Customer("John Doe", "222222-2222");
+
+		// Set up mock behavior
+		when(customerRepo.save(newCustomer)).thenReturn(newCustomer);
+
+		// Perform the request to the controller
+		// Perform the request to the controller
+		mockMvc.perform(post("/customers/sd")
+						.param("name", "John Doe")
+						.param("ssn", "222222-2222"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/customers"));
+
+		// Verify that save method was called with the correct customer object
+		verify(customerRepo).save(newCustomer);
+	}  /*CONTROLLER TESTING CUSTOMERS*/
+
+	@Test
+	public void testAddItem() throws Exception {
+		// Prepare mock data
+		Item newItem = new Item("Example Item", 50.00);
+
+		// Set up mock behavior
+		when(itemRepo.save(newItem)).thenReturn(newItem);
+
+		// Perform the request to the controller
+		mockMvc.perform(post("/items/sd")
+						.param("name", "Example Item")
+						.param("price", "50.00"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/items"));
+
+		// Verify that save method was called with the correct item object
+		verify(itemRepo).save(newItem);
+	}
+
+	@Test
+	public void deleteItemById() throws Exception {
+		// Prepare mock data
+		List<Item> items = new ArrayList<>();
+
+		Item item1 = new Item("Bäverpäls", 1000);
+		item1.setId(1L);
+
+		Item item2 = new Item("Sälpäls", 500);
+		item2.setId(2L);
+
+		items.add(item1);
+		items.add(item2);
+
+		// Set up mock behavior
+		when(itemRepo.findById(2L)).thenReturn(Optional.of(item2));
+		when(itemRepo.findAll()).thenReturn(items);
+
+		// Perform the request to the controller
+		mockMvc.perform(get("/items/2/delete"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("deleteItem.html"))
+				.andExpect(model().attribute("item", item2));
+
+		// Verify that deleteById was called with the correct ID
+		verify(itemRepo).deleteById(2L);
+	}
+	@Test
+	public void testGetAllItem() throws Exception {
+		// Prepare mock data
+		List<Item> items = new ArrayList<>();
+		Item item1 = new Item("Björnpäls", 1000.00);
+		Item item2 = new Item("Pingvinpäls", 1000.00);
+		items.add(item1);
+		items.add(item2);
+
+		// Set up mock behavior
+		when(itemRepo.findAll()).thenReturn(items);
+
+		// Perform the request
+		mockMvc.perform(get("/items"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("items.html"))
+				.andExpect(model().attribute("items", items));
+	}
+	@Test
+	public void testGetItemById() throws Exception {
+		// Prepare mock data
+		TestItem item = new TestItem();
+		item.setId(1L);
+		item.setName("Kamelpäls");
+		item.setPrice(999.00);
+
+		// Set up mock behavior
+		when(itemRepo.findById(1L)).thenReturn(Optional.of(item));
+
+		// Perform the request
+		mockMvc.perform(get("/items/1"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("item.html")) // Change "items.html" to "item.html"
+				.andExpect(model().attribute("item", item));
+	}
+
 }
+   /*CATEGORY CONTROLLER TESTING*/
+
+
+
+
+
 
